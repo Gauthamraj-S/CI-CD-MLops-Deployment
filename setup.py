a27c44c86b070d9f20dc98d9e0b8be8e3e99f65b
Gauthamraj-S <sgauthamraj4@gmail.com>
2024-03-11 12:13:05 +0530

    Adding .csv file and adding the executed and compared models code of jupyter notebook

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from setuptools import find_packages,setup
+from typing import List
 
- 
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readline()
+        requirements=[req.replace("\n","") for req in requirements]
+        if(HYPHEN_E_DOT in requirements):
+            requirements.remove(HYPHEN_E_DOT)s
+    return requirements
 
 setup(
     name='ML project',
@@ -8,6 +18,6 @@ setup(
     author = 'Gautham Raj S',
     author_email='sgauthamraj4@gmail.com',
     packages=find_packages(),
-    install_requires=['pandas','numpy','seaborn']
+    install_requires=get_requirements('requirements.txt')
 
 )
